80d996a521eabc87ef9dc65a15e4d61cb8005d44
johnsmith1500 <jv20052003@gmail.com>
2020-06-25 09:56:29 +0900

    cadastro e envio de dados para o Banco

--- www/components/Banco.js
+++ www/components/Banco.js
@@ -1,2 +1,27 @@
 // This is a JavaScript file
 
+$(document).on("click","#salvar",function(){
+    var parametros = {
+      "nome":$("#nome").val(),
+       "email":$("#email").val(),
+      "senha":$("#senha").val()   
+    }
+    $.ajax({
+      type:"post", //como vai enviar os dados
+      url:"http://localhost:8080/cadastra.php", //pra onde vai enviar os dados
+      data:parametros,//o que será enviado
+
+      //se funcionar
+      success: function(data){
+        navigator.notification.alert(data);
+        $("#nome").val(""),
+        $("#email").val(""),
+        $("#senha").val("")  
+      },
+
+      //se não funcionar
+      error: function(data){
+        navigator.notification.alert("Erro ao cadastrar");
+      }
+    });
+});
